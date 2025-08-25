409eed1034798797bf295e565c7b65bf1207eb96
Jodus-Melodus <philipgricius@gmail.com>
2025-08-25 21:14:35 +0200

    add albumn cover to player

--- js/player.js
+++ js/player.js
@@ -2,11 +2,24 @@ import { updateTab } from "./app.js";
 
 const audioPlayer = document.getElementById("audioPlayer");
 const nowPlaying = document.getElementById("now-playing");
+const player = document.getElementById("player");
+const playerContainer = document.getElementById("player-container");
 
 export function playTrack(track) {
     if ((track.image) && (track.name)) {
         updateTab(track.image, track.name);
     }
+
+    let img = document.getElementById("player-track-cover");
+    if (!img) {
+        img = document.createElement("img")
+        img.alt = "track cover";
+        img.id = "player-track-cover"
+        img.className = "player-track-cover"
+        playerContainer.prepend(img);
+    }
+
+    img.src = track.image;
 
     audioPlayer.src = track.audio;
     audioPlayer.play();
